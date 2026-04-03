5302e85a47d5569ec6805e2a88ea64b705bf6f44
Mesfin Tibebu <mesfinbt@gmail.com>
2026-04-02 21:09:31 -0400

    Converted re_agent to use sequential sub-agents to enforce order of execution.

--- src/requirement/re_agent/requirements_wrapper.py
+++ src/requirement/re_agent/requirements_wrapper.py
@@ -1,14 +1,11 @@
 """Wrapper agent to create a workflow of the RE agents."""
 
 from typing import Optional
-from agents import (add_rag_tool, AgentBase, get_model_from, get_agent_config)
-from utils.constants import (AgentRunMode, ExecMode, DEFAULT_MODEL_NAME, CONTENT_LENGTH_SMALL)
-import time
+from agents import AgentBase
+from utils.constants import (AgentRunMode, ExecMode, DEFAULT_MODEL_NAME)
 
-from google.adk.agents import (Agent)
-from google.adk.tools.agent_tool import AgentTool
+from google.adk.agents import SequentialAgent
 from utils.logger import (get_run_id, setup_logging)
-from prompt_templates import RE_AGENT_SYSTEM_PROMPT
 from requirement import CollectorAgent
 from requirement import AnalyzerAgent
 from requirement import SpecifierAgent
@@ -20,7 +17,7 @@ class RequirementsWrapperAgent(AgentBase):
   def __init__(
       self,
       llm_model_name: str,
-      system_prompt: Optional[str] = RE_AGENT_SYSTEM_PROMPT,
+      system_prompt: Optional[str] = None,
       run_mode: Optional[AgentRunMode] = AgentRunMode.MAIN,
       rag: Optional[bool] = True,
       exec_mode: Optional[ExecMode] = ExecMode.LOCAL,
@@ -36,24 +33,10 @@ class RequirementsWrapperAgent(AgentBase):
         llm_model_name, run_mode=run_mode, rag=rag, exec_mode=exec_mode
     ).get_agent()
 
-  def get_agent(self) -> Agent:
-    tools = []
-
-    if self._rag:
-      add_rag_tool(tools, self._rag, self._exec_mode)
-
-    tools.append(AgentTool(agent=self._collector_agent))
-    tools.append(AgentTool(agent=self._analyzer_agent))
-    tools.append(AgentTool(agent=self._specifier_agent))
-
-    return Agent(
+  def get_agent(self) -> SequentialAgent:
+    return SequentialAgent(
         name="re_agent",
-        model=get_model_from(self._llm_model_name),
-        description="A requirements wrapper agent that uses RE agent tools to generate the SRS.",
-        instruction=self.get_instruction,
-        tools=tools,
-        generate_content_config=get_agent_config(CONTENT_LENGTH_SMALL),
-        output_key="requirements_output",
+        sub_agents=[self._collector_agent, self._analyzer_agent, self._specifier_agent],
     )
 
 
